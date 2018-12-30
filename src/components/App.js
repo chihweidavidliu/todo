3ed4e4da105dcf569a1691552a20e94a2f0f5c6f
Chih-Wei <chihweiliu1993@gmail.com>
2018-12-30 18:55:13 +0000

    Added bottom bar

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,19 @@ import './App.css';
 import FormInput from './FormInput';
 import TodoListContainer from './TodoListContainer';
 import DeleteAll from './DeleteAll';
+import BottomBar from './BottomBar';
 
 const App = (props) => {
   return (
     <div id="main" className="ui container">
       <h1>My To-Dos</h1>
-
       <div className="topbar">
         <h3>Enter todo:</h3>
         <DeleteAll />
       </div>
       <FormInput />
       <TodoListContainer />
+      <BottomBar />
     </div>
   )
 }
